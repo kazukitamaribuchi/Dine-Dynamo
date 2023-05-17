6b10826301c14af27e3db9441bc599d13bbcb65b
kRysTasis <krystasis12@gmail.com>
2023-05-17 20:21:31 +0900

    連携テスト

--- backend/tgm/viewsets.py
+++ backend/tgm/viewsets.py
@@ -1,12 +1,4 @@
-from rest_framework import (
-    authentication,
-    filters,
-    generics,
-    mixins,
-    permissions,
-    status,
-    viewsets,
-)
+from rest_framework import authentication, filters, permissions, status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
