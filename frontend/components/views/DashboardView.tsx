eb4de50b2fdd2b50ad80b3d4c693c02ff090d8b8
kRysTasis <krystasis12@gmail.com>
2023-06-21 10:24:47 +0000

    add: dashboardにtabs追加

--- frontend/components/views/DashboardView.tsx
+++ frontend/components/views/DashboardView.tsx
@@ -10,7 +10,7 @@ import { useGetUserDetail } from "@/hooks/api/useGetUserDetail";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { CommonSidebar } from "../templates/Sidebar";
-import { Layout, Menu, Avatar, Divider, MenuProps } from "antd";
+import { Layout, Menu, Avatar, Divider, MenuProps, Tabs } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
@@ -30,10 +30,51 @@ export default function DashBoardView(props: any) {
   console.log("userDetail: ", userDetail);
   console.log("subscription: ", props);
 
+  // TODO ページ遷移時に全てのタブのデータ取得
+
+  // TODO childrenのコンポーネント化
+
+  const [items, setItems] = useState([
+    {
+      key: "1",
+      label: `サマリー`,
+      children: "Content of Tab Pane 1",
+    },
+    {
+      key: "2",
+      label: `投稿`,
+      children: "Content of Tab Pane 2",
+    },
+    {
+      key: "3",
+      label: `ストーリーズ`,
+      children: "Content of Tab Pane 3",
+    },
+    {
+      key: "4",
+      label: `フォロワー`,
+      children: "Content of Tab Pane 4",
+    },
+    {
+      key: "5",
+      label: `アクション`,
+      children: "Content of Tab Pane 5",
+    },
+    {
+      key: "6",
+      label: `リーチ`,
+      children: "Content of Tab Pane 6",
+    },
+  ]);
+
+  const changeTabs = (key: string) => {
+    console.log("changeTabs", key);
+  };
+
   return (
     <AuthView>
       <BaseDashboardView title="index">
-        <div>Content Here</div>
+        <Tabs items={items} onChange={changeTabs}></Tabs>
       </BaseDashboardView>
     </AuthView>
   );
